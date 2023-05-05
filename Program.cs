097bf31d8796551c6e6213a2820c0f3d4a9fd13d
Rahmatullah <98853284+BOOSR@users.noreply.github.com>
2023-05-05 06:55:54 +0430

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,28 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HMLamba
+namespace week_05
 {
     class Program
     {
-        static int sum(int a, int b) => a + b;
-        static int minis(int a, int b) => a - b;
-        static double devide(double a, double b) =>Convert.ToDouble( a/b);
-        static int multyplex(int a, int b) => a * b;
-
+    static    double final;
+       static int  time_to_reach_goal(double initial_amount, double interest_rate, double goal_amount)
+        {
+            int years = 0;
+            while (initial_amount < goal_amount)
+            {
+                double interest = initial_amount * interest_rate;
+                initial_amount += interest;
+                years++;
+            }
+            final = initial_amount;
+            return years;
+        }
         static void Main(string[] args)
         {
-            int x=10, y = 5;
-            int  s = sum(x, y);
-            int miniss = minis(x, y);
-            double devid = devide(x, y);
-            int mul = multyplex(x, y);
-            Console.WriteLine( $"{x} + {y} : {s}");
-            Console.WriteLine( $"{x} - {y} : {miniss}");
-            Console.WriteLine( $"{x} / {y} : {devid}");
-            Console.WriteLine( $"{x} * {y} : {mul}");
-         
-            Console.ReadKey();
+            Console.WriteLine("What is your current balance?");
+            double initial_amount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your current annul intrest (in %)?");
+            double interest_rate =Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("what balance would you like To have?");
+            double goal_amount;
+            do
+            {
+                goal_amount = Convert.ToDouble(Console.ReadLine());
+                if (goal_amount < initial_amount)
+                {
+                    Console.WriteLine("You must enter an amount greater then your current balance!" +
+                        "\nplease enter another value.");
+                }
+                else break;
+            } while (goal_amount < initial_amount);
+
+
+            int years = time_to_reach_goal(initial_amount, interest_rate, goal_amount);
+
+            Console.WriteLine("In {0} years you'll have a balance of {1}", years,final);
+            Console.ReadLine();
         }
+
     }
 }
