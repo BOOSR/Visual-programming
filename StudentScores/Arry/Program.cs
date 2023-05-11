9584b2919ab18238fcac58f3bd0325ac69f5d3a0
Rahmatullah <98853284+BOOSR@users.noreply.github.com>
2023-05-11 14:25:30 +0430

    Add files via upload

---
+++ StudentScores/Arry/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] math1 = new int[3, 4];
+            int[,] math2 = new int[4, 2] { { 2, 3 }, { 4, 5 }, { 1, 7 },{ 0, 9 } };
+            int[,] math3 = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
+
+            for (int i = 0; i < math1.GetLength(0); i++)
+            {
+                Console.WriteLine("Enter the number  of row {0}", i);
+                for (int j = 0; j < math1.GetLength(1); j++)
+                {
+                    Console.WriteLine("math1[{0},{1}]\t", i,j);
+                    math1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            for ( int i=0;i<math1.GetLength(0); i++)
+            {
+
+                for ( int j = 0; j < math1.GetLength(1); j++)
+                {
+                    Console.Write(math1[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("+++++++++++++");
+
+            int[,]test= new int[3,3];
+            //foreach سطر به سطر
+            int counter = 0;
+            foreach (int a in math3)
+            {
+                counter++;
+                foreach(int b in a)
+                {
+                    counter = 0;
+                    Console.Write( b + '\t');
+
+                }
+
+            }
+
+            Console.WriteLine("\n____________________");
+
+            for (int i = 0; i < math2.GetLength(0); i++)
+            {
+                for (int j = 0; j < math2.GetLength(1); j++)
+                {
+                    Console.Write(math2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+          
+        }
+    }
+}
---
+++ StudentScores/HM/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter the number of lesson's:");
+            int s=Convert.ToInt32( Console.ReadLine());
+            int[] score = new int[s];
+            string[] lessons = new string[s];
+            int sum = 0;
+            double Avg = 0;
+            for ( int i = 0; i < s; i++)
+            {
+                Console.Write("Enter Lesson's Name:");
+                lessons[i] = Console.ReadLine();
+             
+                Console.Write("Enter lEssion's Score:");
+                score[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < lessons.Length; i++)
+            {
+                sum += score[i];
+            }
+            Avg = (double)sum / s;
+
+            Console.WriteLine("  Lessons   |  Score   |  Category |   state   |");
+            Console.WriteLine("|-----------|----------|-----------|-----------|");
+            for ( int i = 0; i < lessons.Length; i++)
+            {
+                Console.Write($"|{lessons[i]}    |{score[i]}        |");
+               
+                switch (lessons[i])
+                {
+                    case string t when t.StartsWith("prog"):
+                        {
+                            Console.Write($"PROGRAMING |{state(score[i])}       |\n");
+                        }break;
+                    case string t when t.StartsWith("math"):
+                        {
+                            Console.Write($"MATH       |{state(score[i])}       |\n");
+                        }break;
+                    case string t when t.StartsWith("islam"):
+                        {
+                            Console.Write($"GENERAL   |{state(score[i])}       |\n");
+                        }break;
+                }
+
+            }
+            Console.WriteLine("|-----------|----------|-----------|-----------|");
+            Console.WriteLine($"|Avrage :{Avg}       ");
+
+
+             string state(int number)
+            {
+                if (number <= 45)
+                {
+                    return "Bad " ;
+                }
+                else if (number>45 && number < 55)
+                {
+                    return "good";
+                }
+                else if (number <=100 && number >= 55)
+                {
+                    return "High";
+                }
+                return "error";
+            }
+ 
+            Console.ReadKey();
+        }
+    }
+}
