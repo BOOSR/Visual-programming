9886765a44c2b06caa2bd090ceec7328a42acc0d
Rahmatullah <98853284+BOOSR@users.noreply.github.com>
2023-05-11 14:29:07 +0430

    Add files via upload

---
+++ week-03/operation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter Your age :");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are "+( (age < 30 ) && (age>20))+ " yaung");
+            Console.WriteLine("You are "+ ((age > 30 )&&(age<50)) +" middle man");
+            Console.WriteLine("You are "+ ( (age == 30 )||(age<=20))+" ok ");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-03/operator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Namespace2;
+//using static System.Console;
+namespace namespace1
+{
+    class C1
+    {
+        public void f1()
+        {
+            Console.WriteLine("you are in  \"Namespace1\" \"1\" and Class \"C1\" and function \"f1\"");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           namespace1.C1 obj = new namespace1.C1();
+            Namespace2.C1 obj2 = new Namespace2.C1();
+            
+            obj.f1();
+            obj2.f1();
+            Console.ReadLine();
+
+            //  int v1 = 3, v2 = 5, v3 = 7;
+            //int re;
+
+            ////re = (v1++ - v2) + (--v3 * (v2 / v1));
+            ////Console.WriteLine($" v1:{v1} \n v2:{v2} \n v3:{v3} \n {re}");
+            ////Console.ReadLine();
+            //string ans;
+            //Console.WriteLine("ENter your name :");
+            //ans = Console.ReadLine();
+            //Namespace2.A.print(ans);
+            //Console.ReadLine();
+
+        }
+    }
+}
+namespace Namespace2 {
+    class C1
+    {
+        public void f1()
+        {
+            Console.WriteLine("you are in  \"Namespace2\" \"2\" and Class \"C1\" and function \"f1\"");
+        }
+    }
+    //class A
+    //    {
+    //        static public void print(string x)
+    //        {
+    //            Console.WriteLine(x);
+    //        }
+    //    }
+}
+
