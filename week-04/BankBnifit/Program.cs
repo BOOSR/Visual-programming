69d990b680ed713c415d21853890dc7c81e0b216
Rahmatullah <98853284+BOOSR@users.noreply.github.com>
2023-05-11 15:21:23 +0430

    Add files via upload

---
+++ week-04/BankBnifit/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBnifit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double blc, bg, bb;
+            int monts_Waite = 0;
+            Console.WriteLine("enter blance");
+            blc = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter Gold");
+            bg = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter bank benifite");
+            bb = Convert.ToDouble(Console.ReadLine());
+
+            while (blc <= bg)
+            {
+                blc = (blc * (bb / 100)) + blc;
+                monts_Waite++;
+           
+            }
+            Console.Write("your blance after\t" + monts_Waite + "\t is \t" + blc);
+            // Console.Write("your blance after one month: ");
+            // bg = (blc * (bb / 100)) + blc;
+            Console.Write("number of mounths they should waite is \t" + monts_Waite);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ week-04/beranching/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace beranching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            char op;
+
+            string reg;
+            Console.WriteLine("enter request :");
+            reg = Console.ReadLine();
+           Array c= reg.Split('+', '-', '*', '/');
+            Console.WriteLine(c);
+            //Console.Write("Enter first number:");
+            //a = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter first number:");
+            //b = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter operator:");
+            //op = Convert.ToChar(Console.ReadLine());
+
+            //if (op == '+')
+            //    Console.WriteLine(a + b);
+            //else if (op == '-')
+            //    Console.WriteLine(a - b);
+            //else if (op == '*')
+            //    Console.WriteLine(a * b);
+            //else if
+            //    (op == '/')
+            //    if(b==0)
+            //    Console.WriteLine(a / b);
+            //else
+            //    Console.WriteLine("error");
+
+
+            //switch (op)
+            //{
+            //    case '+':
+            //        Console.WriteLine(a + b);
+            //        break;
+            //    case '-':
+            //        Console.WriteLine(a - b);
+            //        break;
+            //    case '*':
+            //        Console.WriteLine(a * b);
+            //        break;
+            //    case '/':
+            //        Console.WriteLine(a / b);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Error");
+            //        break;
+            //}
+
+            Console.ReadLine();
+
+            //fds
+            //fds
+        }
+    }
+}
