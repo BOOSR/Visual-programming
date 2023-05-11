98c1e0367af281bff56c537c615d10b1d737aca7
Rahmatullah <98853284+BOOSR@users.noreply.github.com>
2023-05-11 14:29:40 +0430

    Add files via upload

---
+++ week-04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double a, b, c, d, e;
+            a = 2;
+            b = 3;
+            c = 4;
+            d = 5;
+            e = 6;
+            Console.WriteLine("a=2  b=3  c=4  d=5 e=6 ");
+            Console.WriteLine("\na * b :"+a * b);
+            Console.WriteLine("\na * b / c :" + a * b / c);
+            Console.WriteLine("\na * b / c * d :" + a * b / c * d);
+            Console.WriteLine("\na * b / c * d / e :" + a * b / c * d / e);
+            Console.ReadLine();
+       
+        }
+    }
+}
